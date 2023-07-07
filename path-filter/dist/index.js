7e7bbf4d29a06e19164531edabf81afa4ad96dc1
MrSdk <mrsdk1902@gmail.com>
2023-07-07 12:08:01 +0500

    test4

--- path-filter/dist/index.js
+++ path-filter/dist/index.js
@@ -11900,7 +11900,7 @@ async function githubComparison() {
     });
     core.debug("-NOT Found error step - 6");
     const diffs = await octokit.paginate(compare_opts);
-    core.debug("-NOT Found error step - 7");
+    core.debug("-NOT Found error step - 7, Keys: " + JSON.stringify({ base, head, owner, repo }));
     const files = new Set();
     for await (const diff of diffs) {
         for (const file of diff.files) {
