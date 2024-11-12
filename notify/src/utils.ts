7d355fb33487dd0002aa1d6b6eda8bfaa7e509d6
Turcheg <zafar.turkmenov@gmail.com>
2024-11-12 05:20:49 +0500

    Fix working with http proxy

--- notify/src/utils.ts
+++ notify/src/utils.ts
@@ -1,6 +1,7 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { ErrorTypes } from "./reference/errors";
 import { TG_MESSAGE_MAX_LENGTH } from "./configs/"
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
 export const send_notification = async function ( 
   url: string, 
@@ -10,8 +11,13 @@ export const send_notification = async function (
   if ( options.text.length > TG_MESSAGE_MAX_LENGTH ) {
     throw new Error( ErrorTypes.message_length_too_long )
   }
+  const axiosOptions: AxiosRequestConfig = {};
+  if ( process.env.HTTPS_PROXY ) {
+    axiosOptions.proxy = false;
+    axiosOptions.httpsAgent = new HttpsProxyAgent(process.env.HTTPS_PROXY);
+  }
 
-  return axios.post( url, options );;
+  return axios.post( url, options, axiosOptions );
 }
 
 export const stringify = ( data: any ) => JSON.stringify( data )
