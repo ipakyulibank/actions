f66078092d5d04ed4bac648827cb17696efda6fa
Shyngys Akhmet <shyngys.akhmet@csssr.com>
2024-12-09 18:12:36 +0500

    Change eslint config got path-filter

--- path-filter/index.ts
+++ path-filter/index.ts
@@ -1,14 +1,19 @@
 import main from './src/main';
 import * as core from '@actions/core';
+
 (async () => {
   try {
-    core.debug("Process started...")
-    
+    core.debug("Process started...");
+
     const result = await main();
     core.setOutput('result', result);
-  } catch (error: any) {
-    core.setFailed(error?.message);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else {
+      core.setFailed(String(error));
+    }
   } finally {
-    core.debug("Process finished...")
+    core.debug("Process finished...");
   }
 })();
