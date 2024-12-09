7ee0aa6bdb7bb35728adbd480889223cb9d946ca
Shyngys Akhmet <shyngys.akhmet@csssr.com>
2024-12-09 18:28:32 +0500

    Add eslint-disable for source files

--- path-filter/src/__tests__/main.spec.ts
+++ path-filter/src/__tests__/main.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { GithubEventTypes } from '../reference/constants';
 import { mockActionCore, mockActionGithub } from '../../testHelpers';
 
